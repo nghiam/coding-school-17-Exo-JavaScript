e74c9a3ff25805a390579d1584668ed456efadd1
unknown <mai.nghia9@gmail.com>
2021-01-26 10:19:36 +0100

    exo2

--- exerciceJavascriptBoucleForeach/public/js/main.js
+++ exerciceJavascriptBoucleForeach/public/js/main.js
@@ -3,3 +3,6 @@
 let array = ['Bilal','Cem','Gauthier','Laurie','Nghia','Yahya','Yassin','Abdel','Mahad','Mohamed','Redwane','Camille','Amine','Antoine','Ayoub','Salim','Said','Yasmina'];
 
 array.forEach(console.log(element));
+
+
+// Exo2
