4177cdc1ca44e66201b05f730a739b17459681cc
unknown <mai.nghia9@gmail.com>
2021-01-26 11:13:25 +0100

    arrivée à l'exo5 Foreach2

--- exerciceJavascriptBoucleForeach/public/js/main.js
+++ exerciceJavascriptBoucleForeach/public/js/main.js
@@ -1,8 +1,12 @@
 // Exo1
 
-let array = ['Bilal','Cem','Gauthier','Laurie','Nghia','Yahya','Yassin','Abdel','Mahad','Mohamed','Redwane','Camille','Amine','Antoine','Ayoub','Salim','Said','Yasmina'];
+// let array = ['Bilal','Cem','Gauthier','Laurie','Nghia','Yahya','Yassin','Abdel','Mahad','Mohamed','Redwane','Camille','Amine','Antoine','Ayoub','Salim','Said','Yasmina'];
 
-array.forEach(console.log(element));
+// array.forEach( element => console.log("Bonjour " + element));
 
 
 // Exo2
+
+// let array = [ 1,2,3,4,5];
+
+// array.forEach (element => console.log(element));
--- exerciceJavascriptBoucleForeach2/public/js/main.js
+++ exerciceJavascriptBoucleForeach2/public/js/main.js
@@ -1,79 +1,45 @@
-// 1. ToDo
+// Exo1
 
-// let jour = prompt("Entrez un jour de la semaine:")
+// let multiplicateur = parseInt(prompt("Veuillez ajouter une valeur:"));
 
-//   switch (jour) {
-//     case "lundi":
-//       console.log("On est lundi. Tu dois garder les enfants ce soir");
-//       break;
-//     case "mardi":
-//       console.log("On est mardi. Tu as football");
-//       break;
-//     case "mercredi":
-//       console.log("On est mercredi. Tu es en congé l'après-midi");
-//       break;
-//     case "jeudi":
-//       console.log("On est jeudi. Tu as cours sur le Discord de Molengeek");
-//       break;
-//     case "vendredi":
-//       console.log("On est vendredi. Tu as cours sur le Discord Molengeek puis tu dois jouer à Among Us. ");
-//       break;
-//     case "samedi":
-//       console.log("On est samedi. Tu n'as pas cours et tu es en congé ");
-//       break;
-//     default:
-//       console.log("Pas de jour pris en compte")
+// for (let index = 0; index <= 10; index++) {
+  
+//   console.log(multiplicateur+"*"+index+"="+(multiplicateur*index));
+    
+// }
+
+// Exo2
+
+// let multiplicateur = parseInt(prompt("Veuillez ajouter une valeur:"));
+
+// for (let index = 0; index <= 10; index=index+2) {
+  
+//   console.log(multiplicateur+"*"+index+"="+(multiplicateur*index));
+    
+// }
+
+// Exo3
+
+
+// for (let index = 20; index >= 0; index=index-2) {
+  
+//   console.log(index);
+// }
+
+// Exo4
+
+// let array = ['Bilal','Cem','Gauthier','Laurie','Nghia','Yahya','Yassin','Abdel','Mahad','Mohamed','Redwane','Camille','Amine','Antoine','Ayoub','Salim','Said','Yasmina'];
+
+// let longPrenoms = [];
+
+// for (let index = 0; index < array.length; index++) {
+
+//   let prenom = array[index];
+//   if ( prenom.length > 5) {
+//     longPrenoms.push(prenom);
 //   }
+// }
+// console.log(longPrenoms)
 
-// 2. Sortez couverts
 
-// let meteo = prompt("Quel temps fait-il aujourd'hui?")
-
-//   switch(meteo){
-//     case "beau":
-//       console.log("il faut beau aujourd'hui");
-//       break;
-//     case "mauvais":
-//       console.log("le temps n'est pas top aujourd'hui");
-//       break;
-//     default:
-//       console.log("Ce n'est pas pris en compte")
-//   }
-
-// 3. Les 2 font la pair
-
-let nombre = parseInt(prompt("donnez un nombre:"));
-switch (Math.abs(nombre % 2)) {
-    case 1:
-        console.log(`${nombre} est un nombre impair`);
-        break;
-    case 0:
-        console.log(`${nombre} est un nombre pair`);
-        break;
-    default:
-        console.log(`${nombre} n'est pas un nombre`);
-}
-
-// 4. Maths "avancée"
-
-let nombre = parseInt(prompt("donne moi un nombre"));
-   let operation = prompt("donne moi une opérateur mathématique");
-   let nombre2 = parseInt(prompt("donne moi un second nombre"));
-
-   switch (operation) {
-      case "*":
-        alert(nombren*ombre2)
-      break;
-      case"+":
-        alert(nombre+nombre2)
-      break;
-      case"/":
-        alert(nombre/nombre2)
-      break;
-      case"-":
-        alert(nombre-nombre2)
-      break;
-      default:
-        alert("Erreur!")
-      break;
-    }
+// Exo5
