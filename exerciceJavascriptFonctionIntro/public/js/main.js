d75bc44a9632837d68a1ed57f90729c6a8c4177d
unknown <mai.nghia9@gmail.com>
2021-01-28 17:01:32 +0100

    Ex8 fonction intro

---
+++ exerciceJavascriptFonctionIntro/public/js/main.js
@@ -0,0 +1,62 @@
+let direBonjour = (prenom, nom) => {
+    console.log(`Bonjour ${prenom} ${nom}!`);
+};
+direBonjour("Nghia","Mai");
+
+// Exo1
+
+function addition(nombre, nombre1) {
+    return nombre + nombre1;
+};
+console.log(addition(2,7));
+
+
+// Exo2
+
+function soustraction(nombre, nombre1) {
+    return nombre - nombre1;
+};
+console.log(soustraction(20,7));
+
+// Exo3
+
+function multiplication(nombre, nombre1) {
+    return nombre * nombre1;
+};
+console.log(multiplication(5,9));
+
+// Exo4
+
+function division(nombre, nombre1) {
+    return nombre / nombre1;
+};
+console.log(division(36,3));
+
+// Exo5
+
+function modulo(nombre, nombre1) {
+    return nombre % nombre1;
+};
+console.log(modulo(12,5));
+
+// Exo6
+
+function carre(nombre) {
+    return nombre * nombre;
+};
+console.log(carre(5));
+
+// Exo7
+
+function exposant(nombre, nombre1) {
+    return  Math.pow(nombre,nombre1);
+};
+console.log(exposant(2,3));
+
+// Exo8
+
+function capitalize(mot) {
+    return mot;
+};
+console.log();
+
--- exerciceJavascriptWhileDoWhile/public/js/main.js
+++ exerciceJavascriptWhileDoWhile/public/js/main.js
@@ -3,19 +3,19 @@
 // Exo1
 
 // let valeur;
-
-// while (!valeur) {
-//    valeur = prompt("Entrez à nouveau une valeur :");
-// }
-
 // do {
-//     valeur = prompt("Entrez un nouveau chiffre :")
-// } while ( valeur < 10);
+//     valeur = prompt("Entrez une valeur");
+// }while (!valeur.trim()); 
 
-// let valeur = 0;
-// while (valeur < 100) {
-//     console.log(valeur);
-//     valeur++;
+// let chiffre
+// do {
+//     chiffre = prompt("Entrez un chiffre plus grand que 10")
+// } while ( chiffre < 10 || isNaN(chiffre));
+
+// let i = 1
+// while (i) {
+//     console.log(i);
+//     i++;
 // }
 
 // let valeur = 3;
@@ -32,13 +32,61 @@
 
 // Exo2
 
-let panierLegumes = ["a","b","c","d"];
+// let panierLegumes = ["a","b","c","d"];
 
-let caisseLegumes = [];
+// let caisseLegumes = [];
 
-let Legumes = [];
 
-while (caisseLegumes.length > 4) {
-    console.log(panierLegumes.shift());
-    console.log(Legumes.push(caisseLegumes));
-}
+// while (panierLegumes.length > 0) {
+    
+    
+//     caisseLegumes.push(panierLegumes.shift());
+
+// }
+// console.log(caisseLegumes);
+// console.log(panierLegumes);
+
+// Exo3
+
+// let question = prompt("Qui est le plus bg de la C17?");
+// let reponse = "Mahad"
+// while (question !== reponse) {
+//     question = prompt("Recommencez svp! Qui est le plus bg de la C17");
+// }
+// alert("Félicitations!");
+
+
+// Exo4
+
+// let enigme 
+// do {
+//     enigme = prompt("De quelle couleur est le cheval de Napoleon?");
+
+// } while (enigme !==);
+// alert("Bonne réponse");
+
+// Exo5
+
+let annee = 2021;
+let randomYear = Math.floor(Math.random() * (annee - 1970)) + 1970;
+
+let question 
+
+let i = 1
+
+let reponse = annee - randomYear;
+
+do {
+    question = prompt("Si je suis né(e) en "+randomYear+",quel âge ai-je aujourd’hui ?");
+    i++;
+} while (question != reponse && i <= 3);
+
+if (question == reponse) {
+    alert("Bonne réponse")
+}else {
+    
+} alert("C'est pas bon!!")
+
+
+// Ex6
+
