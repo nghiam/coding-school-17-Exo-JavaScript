7f5f18974103a1341c382429a40a12ca0a2c84ce
unknown <mai.nghia9@gmail.com>
2021-01-29 12:49:26 +0100

    Exo1 + Exo2 : Fonctions Mid

---
+++ exerciceJavascriptFonctionsMiddle/public/js/main.js
@@ -0,0 +1,49 @@
+// Exo 1 : reverseNumber
+
+
+// let reverseNumber = (nombre) =>{
+//     nombre = nombre + ""
+//     return nombre.split("").reverse().join("")
+// }
+// console.log(reverseNumber(851));
+
+// Exo 2 : Pair
+
+// Version IF ELSe
+// function estPair(nombre){
+// if ( nombre % 2 == 0 ) {
+//     console.log("Ton nombre est pair");
+// } else {
+//     console.log("Ton nombre est impair");
+// }
+// }
+// estPair(11);
+
+// Version SWITCH
+// function estPair(nombre){
+//     switch (nombre % 2) {
+//         case 0 :
+//             console.log("Le nombre " +nombre+ " est pair, il est divisible par 2");
+//             break;
+//         default:
+//             console.log("Ce nombre est impair, il n'est pas divisible par 2.");
+//             break;
+//     }
+// }
+// estPair(12);
+
+
+// Exo 3 : LogIn
+
+let index;
+function logIn(mdp){
+    if (mdp == index) {
+        alert("Vous êtes bien connecté")
+    } else {
+        
+    }
+}
+
+let logIn = (mdp) => {
+
+}
