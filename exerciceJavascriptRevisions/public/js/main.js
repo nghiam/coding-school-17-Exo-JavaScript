671502f49369cf831c7278db28869d1ad17ecadb
unknown <mai.nghia9@gmail.com>
2021-02-01 12:45:02 +0100

    Exo1 : done + Exo2 : n5

--- exerciceJavascriptRevisions/public/js/main.js
+++ exerciceJavascriptRevisions/public/js/main.js
@@ -1,17 +1,69 @@
 // Exo 1
 
+//1.
 // let prenom = prompt("Entrez un prénom : ")
 
 // alert(prenom.charAt(0));
 
-// Exo 2
+//2.
 
 // let prenom = prompt("Entrez un prénom : ")
 
 // alert(prenom.substr(1));
 
-// Exo 3 
+//3. 
 
-let prenom = prompt("Entrez un prénom : ")
+// let prenom = prompt("Entrez un prénom : ")
+// alert(prenom.charAt(4));
 
-alert()
+// if(prenom.length <= 4) {
+// alert("Votre prénom est trop court")
+// } 
+
+//4.
+
+// let prenom = prompt("Entrez un prénom : ")
+
+// if(prenom.length <= 4) {
+// alert(prenom.charAt(prenom.length-1));
+// alert("Votre prénom est trop court et il n'était qu'à " + prenom.length + " caractères")
+// } else alert(prenom.charAt(4));
+
+// 5.
+
+// let prenom = prompt("Entrez un prénom :")
+
+// alert(prenom.toLowerCase());
+// alert(prenom.toUpperCase());
+// alert(prenom.charAt(0).toUpperCase() + prenom.substr(1).toLowerCase());
+// alert(prenom.charAt(0).toLowerCase() + prenom.substr(1).toUpperCase());
+
+// ________________________________________________________________________
+
+// Exo 2
+
+// 1.
+// let tableau = ["abricot","banane","canard","doigt","element"];
+
+// console.log(tableau[2]);
+
+// 2.
+// let tableau = ["abricot","banane","canard","doigt","element"];
+
+// console.log(tableau[0] , tableau[4]);
+
+// 3.
+// let tableau = ["abricot","banane","canard","doigt","element"];
+
+// console.log(tableau[1].replace('e','').toUpperCase()+ tableau[1].charAt(tableau[1].length - 1).toLowerCase());
+
+// 4.
+// let tableau = ["abricot","banane","canard","doigt","element"];
+
+// console.log(tableau[3].charAt(0));
+
+// 5.
+// let tableau = ["abricot","banane","canard","doigt","element"];
+
+// console.log(tableau[4].replace('t',''));
+
