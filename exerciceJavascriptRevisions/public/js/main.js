28ff7e28435c67ed26e969c0d1d9f6f6cbb78945
unknown <mai.nghia9@gmail.com>
2021-02-02 15:22:21 +0100

    Exo 3 Part III : done + Exo 4 Part I : debut

--- exerciceJavascriptRevisions/public/js/main.js
+++ exerciceJavascriptRevisions/public/js/main.js
@@ -128,7 +128,7 @@
 // "Harry",
 // ]
 
-// console.log(tableau);
+
 
 
 // 2.
@@ -147,8 +147,11 @@
 
 // for (let index = 0; index <= 8; index++) {
 //     console.log(tableau[index]);
-    
 // }
+    
+// tableau.forEach((element) => {
+//     console.log(element);
+// });
 
 // 3.
 
@@ -168,6 +171,10 @@
 //     console.log(tableau[index].charAt(0));
 // }
 
+// tableau.forEach(element => {
+//     console.log(element.substring(0,1));
+// });
+
 // 4.
 
 // let tableau = [
@@ -186,6 +193,10 @@
 //     console.log(tableau[index].charAt(1));
 // }
 
+// tableau.forEach(element=> {
+//     console.log(element.charAt(1));
+// })
+
 // 5.
 
 // let tableau = [
@@ -203,6 +214,10 @@
 // for (let index = 0; index <= 8; index++) {
 //     console.log(tableau[index].charAt(1) + tableau[index]);
 // }
+
+// tableau.forEach(element=> {
+//     console.log(element.charAt(1) + element);
+// })
 
 // 6.
 
@@ -224,6 +239,19 @@
 //         console.log(tableau[index]);
 //     }
 // }
+
+// tableau.forEach(element => {
+//     if (tableau.indexOf(element)%2 == 0) {
+//         console.log(element);
+//     }
+// });
+
+// tableau.forEach(element, index) => {
+//     if (index % 2 == 0) {
+//         console.log(element);
+//     }
+// });
+
 
 // ________________________________________________________________________
 // Exo 3 : Part II 
@@ -264,27 +292,39 @@
 //     }
 // }
 
+// tableau.forEach((element, index) => {
+//     if (index % 2 == 1) {
+//         console.log(element.toUpperCase());
+//     }
+// });
+
 
 // 3.
 
 // let tableau = [
-// "Mick",
-// "Patrick",
-// "Homer",
-// "Mehdi",
-// "Witch",
-// "Poulard",
-// "Amen",
-// "Ludovic",
-// "Harry",
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
 // ]
 
 // for (let index = 0; index < tableau.length ; index++) {
 
 //     if (index % 2 == 0) {
-//         console.log(tableau[index].charAt(0).toUpperCase() + tableau[index].substr(1));
+//         console.log(tableau[index].charAt(0).toUpperCase() + tableau[index].substring(1));
 //     }
 // }
+
+// tableau.forEach((element, index) => {
+//     if (index % 2 == 0) {
+//         console.log(element.charAt(0).toUpperCase() + element.slice(1));
+//     }
+// });
 
 // 4.
 
@@ -308,26 +348,40 @@
 //     }
 // }
 
+// tableau.forEach((element, index) => {
+//     if (index % 2 == 1 && element.length > 4) {
+//         console.log(element);
+//     }
+// });
+
 // 5. 
 
 // let tableau = [
-// "Mick",
-// "Patrick",
-// "Homer",
-// "Mehdi",
-// "Witch",
-// "Poulard",
-// "Amen",
-// "Ludovic",
-// "Harry",
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
 // ]
 
 // for (let index = 0; index < tableau.length ; index++) {
 
 //     if (index % 2 == 0 && tableau[index].length > 4) {
-//         console.log(tableau[index].charAt(0).toUpperCase() + tableau[index].substr(1));
+//          console.log(tableau[index].charAt(0).toUpperCase());
 //     }
 // }
+
+// tableau.forEach((element, index) => {
+//     if (index % 2 == 0 && element.length > 4) {
+//         console.log(element.charAt(0).toUpperCase());
+//     }
+// });
+
+
 
 // 6.
 
@@ -343,16 +397,37 @@
 // "harry",
 // ]
 
-// // if (['a', 'e', 'm', 'f', 'y', 'n'].includes(prenom[0])) {
+// if (['a', 'e', 'm', 'f', 'y', 'n'].includes(prenom[0])) {
 
 // for (let index = 0; index < tableau.length; index++) {
-//     let prenom = 0;
+    
 //     if (tableau[index].charAt(0) == "a" || tableau[index].charAt(0) == "e" || tableau[index].charAt(0) == "m" || tableau[index].charAt(0) == "f" || tableau[index].charAt(0) == "y" || tableau[index].charAt(0) == "n")
 
 
 //     console.log(tableau[index]); 
 // }
 
+
+// tableau.forEach((element) => {
+// let e = element.charAt(0)
+// if ( e == "a" || e   == "e" || e   == "m" ||  e   == "f"  || e  == "y" || e   == "n") {
+//   console.log(element);
+// }
+// });
+
+// SWITCH
+// tableau.forEach((element) => {
+// let a = element.charAt(0)
+// switch (a) {
+//     case "a":
+//     case "e":
+//     case "m":
+//     case "f":
+//     case "y":
+//     case "n":
+//         console.log(element);
+//         break; 
+// }
 
 // 7.
 
@@ -370,12 +445,180 @@
 
 // for (let index = 0; index < tableau.length; index++) {
 //     let prenom = 0;
-//     if (tableau[index].length > 5 && tableau[index].charAt(0) == "a" || tableau[index].charAt(0) == "e" || tableau[index].charAt(0) == "m" || tableau[index].charAt(0) == "f" || tableau[index].charAt(0) == "y" || tableau[index].charAt(0) == "n") {
+//     if (tableau[index].length <= 5 && tableau[index].charAt(0) == "a" || tableau[index].charAt(0) == "e" || tableau[index].charAt(0) == "m" || tableau[index].charAt(0) == "f" || tableau[index].charAt(0) == "y" || tableau[index].charAt(0) == "n") {
 //         console.log(tableau[index]);
 //     }
 // }
+
+// tableau.forEach((element) => {
+// let e = element.charAt(0)
+// if ( element.length > 5 && (e == "a" || e   == "e" || e   == "m" ||  e   == "f"  || e  == "y" || e   == "n")) {
+//   console.log(element);
+// }
+// });
 
 
 // Exo 3 : Part III
 
 // 1.
+
+// let tableau = [
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
+// ]
+
+// 2.
+
+
+// let tableau = [
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
+// ]
+
+
+// tableau.forEach(e => {
+//     if (e.length < 5 && ( e.charAt(0) == "a"|| e.charAt(0) == "e" || e.charAt(0) == "m" || e.charAt(0) == "f" || e.charAt(0) == "y" || e.charAt(0) == "n")) {
+//         console.log(e[0].toUpperCase() + e.slice(1)); // [0] == charAt(0)
+//     }
+// });
+
+// 3.
+
+// let tableau = [
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
+// ]
+
+// tableau.forEach((e,index) => {
+//     if (e.length < 5 && ( e.charAt(0) == "k"|| e.charAt(0) == "c" || e.charAt(0) == "m" || e.charAt(0) == "f" || e.charAt(0) == "y" || e.charAt(0) == "n" || e.charAt(0) == "a" || e.charAt(0) == "e" || e.charAt(0) == "p" || e.charAt(0) == "o")) {
+//         if ( index % 2 == 1)
+//         console.log(e[0].toUpperCase() + e.slice(1)); // [0] == charAt(0)
+//     }
+// });
+
+// 4.
+
+// let tableau = [
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
+// ]
+
+// tableau.forEach((e,index) => {
+//     if (e.length < 5 && ( e.charAt(0) == "k"|| e.charAt(0) == "c" || e.charAt(0) == "m" || e.charAt(0) == "f" || e.charAt(0) == "y" || e.charAt(0) == "n" || e.charAt(0) == "a" || e.charAt(0) == "e" || e.charAt(0) == "p" || e.charAt(0) == "o")) {
+//         if ( index % 2 == 0)
+//         console.log(e[0].toLowerCase() + e[1].toUpperCase() + e[e.length-1].toUpperCase()); // [0] == charAt(0)
+//     }
+// });
+
+// 5.
+
+// let tableau = [
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
+// ]
+
+// let res = "";
+
+// tableau.forEach((e,index) => {
+//     if (e.length < 7 && ( e.charAt(0) == "k"|| e.charAt(0) == "c" || e.charAt(0) == "m" || e.charAt(0) == "f" || e.charAt(0) == "y" || e.charAt(0) == "n" || e.charAt(0) == "a" || e.charAt(0) == "e" || e.charAt(0) == "p" || e.charAt(0) == "o")) {
+//         if ( index % 2 == 0)
+//         res += e[0].toUpperCase() + e[1].toLowerCase();
+//     }
+// })
+// console.log(res); 
+
+
+// Exo 4 Part I
+
+// 1.
+
+let leTableau = [
+    4,
+    "true",
+    true,
+    "bonjour",
+    511,
+    "coding school",
+    {},
+    2020,
+    1 ,
+    "maryam",
+    false,
+    [],
+    13,
+    ["salut",13,false],
+    "les tartines c'est délicieux",
+    "true et false",
+    1070,
+    478,
+    "hello",
+    "top",
+    4000,
+    99,
+    "codeur",
+    45,
+    "javascript",
+    399,
+    "autodidacte",
+    "oui",
+    1000,
+    1001,
+    "brainstorming",
+    "lol"];
+
+
+
+//     leTableau.forEach(element => {
+        
+//         console.log("l'element " + element + " est de type " +typeof(element));
+//     });
+
+
+// 2.
+
+let lesIntegers = [];
+let lesStrings = [];
+let lesAutres = [];
+
+
+lesIntegers.forEach(element => {
+    leTableau.slice(typeof(int)) += lesIntegers
+
+});
+console.log(lesIntegers);
+console.log(leTableau);
