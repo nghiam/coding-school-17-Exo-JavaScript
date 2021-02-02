e738cd4c00ba373b752c13a4b4e748983027d3f1
unknown <mai.nghia9@gmail.com>
2021-02-02 10:56:40 +0100

    Exo 3 part II : Done + début Exo 3 Part III

--- exerciceJavascriptRevisions/public/js/main.js
+++ exerciceJavascriptRevisions/public/js/main.js
@@ -1,42 +1,50 @@
-// Exo 1
+//  Exo 1
 
-//1.
+// 1.
 // let prenom = prompt("Entrez un prénom : ")
 
 // alert(prenom.charAt(0));
 
-//2.
+// 2.
 
 // let prenom = prompt("Entrez un prénom : ")
 
-// alert(prenom.substr(1));
+// alert(prenom.substring(1));
 
-//3. 
+// 3. 
 
 // let prenom = prompt("Entrez un prénom : ")
-// alert(prenom.charAt(4));
 
 // if(prenom.length <= 4) {
-// alert("Votre prénom est trop court")
-// } 
+    // alert("Votre prénom est trop court")
+    // } else {
+    // alert(prenom.charAt(4));
+//}
 
-//4.
+// 4.
 
 // let prenom = prompt("Entrez un prénom : ")
 
 // if(prenom.length <= 4) {
 // alert(prenom.charAt(prenom.length-1));
 // alert("Votre prénom est trop court et il n'était qu'à " + prenom.length + " caractères")
-// } else alert(prenom.charAt(4));
+// } else alert("Voici la dernière lettre de votre prénom : " + prenom.charAt(prenom.length - 1));
+
+
 
 // 5.
 
 // let prenom = prompt("Entrez un prénom :")
 
-// alert(prenom.toLowerCase());
-// alert(prenom.toUpperCase());
-// alert(prenom.charAt(0).toUpperCase() + prenom.substr(1).toLowerCase());
-// alert(prenom.charAt(0).toLowerCase() + prenom.substr(1).toUpperCase());
+
+// console.log(prenom.toLowerCase());
+// console.log(prenom);
+// console.log(prenom.toUpperCase());
+// console.log(prenom);
+// console.log(prenom.charAt(0).toUpperCase() + prenom.slice(1).toLowerCase());
+// console.log(prenom);
+// console.log(prenom.charAt(0).toLowerCase() + prenom.slice(1).toUpperCase());
+
 
 // ________________________________________________________________________
 
@@ -50,51 +58,57 @@
 // 2.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
-// console.log(tableau[0] , tableau[4]);
+// console.log(tableau[0],tableau[tableau.length-1]);
 
 // 3.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
 // console.log(tableau[1].replace('e','').toUpperCase()+ tableau[1].charAt(tableau[1].length - 1).toLowerCase());
 
+// corr : console.log(tableau[1].substring(0, tableau[1].length-1).toUpperCase() + tableau[1].slice(-1).toLowerCase());
+
 // 4.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
 // console.log(tableau[3].charAt(0));
 
+
 // 5.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
 // console.log(tableau[4].replace('e',''));
+// corr : console.log(tableau[4].slice(1));
 
 // 6.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
-// console.log(tableau[0].substr(0,2));
+// console.log(tableau[0].substring(0,2));
+// corr : console.log(tableau[0].substring(0,1).toUpperCase()+tab[0].substring(1,2).toLowerCase);
 
 // 7.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
-// console.log(tableau[0] +" "+ tableau[2]);
+// console.log(tableau[0] + tableau[2]);
 
 // 8.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
-// console.log( tableau[1].substr(0,3) + tableau[4].substr(3));
+// console.log( tableau[1].substring(0,Math.round(tableau[1].length / 2)) 
+// + tableau[tableau.length-1].substring(Math.round(tableau[tableau.length-1]) / 2));
 
 // 9.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
-// console.log(tableau[2].substr(0,2).toUpperCase() + tableau[4] );
+// console.log(tableau[3].substring(0,2).toUpperCase() , tableau[4].toLowerCase() );
 
 // 10.
 // let tableau = ["abricot","banane","canard","doigt","element"];
 
 
-// console.log(tableau[0].substr(0,2).charAt(0).toLowerCase() + tableau[0].substr(0,2).charAt(1).toUpperCase()  +" "+ tableau[1].substr(0,2).charAt(0).toLowerCase() + tableau[1].substr(0,2).charAt(1).toUpperCase() +" "+ tableau[2].substr(0,2).charAt(0).toLowerCase() + tableau[2].substr(0,2).charAt(1).toUpperCase() +" "+ tableau[3].substr(0,2).charAt(0).toLowerCase() + tableau[3].substr(0,2).charAt(1).toUpperCase() +" "+ tableau[4].substr(0,2).charAt(0).toLowerCase() + tableau[4].substr(0,2).charAt(1).toUpperCase());
+// console.log(tableau[0].substring(0,2).charAt(0).toLowerCase() + tableau[0].substring(0,2).charAt(1).toUpperCase()  +" "+ tableau[1].substring(0,2).charAt(0).toLowerCase() + tableau[1].substring(0,2).charAt(1).toUpperCase() +" "+ tableau[2].substring(0,2).charAt(0).toLowerCase() + tableau[2].substring(0,2).charAt(1).toUpperCase() +" "+ tableau[3].substring(0,2).charAt(0).toLowerCase() + tableau[3].substring(0,2).charAt(1).toUpperCase() +" "+ tableau[4].substring(0,2).charAt(0).toLowerCase() + tableau[4].substring(0,2).charAt(1).toUpperCase());
 
 // for (let i = 0; i <= 4; i++) {
-//     console.log(tableau[i].substr(0,2).charAt(0).toLowerCase() + tableau[i].substr(0,2).charAt(1).toUpperCase());
+//  console.log(tableau[i].substring(0,2).charAt(0).toLowerCase() + tableau[i].substring(0,2).charAt(1).toUpperCase());
     
 // }
 // ______________________________________________________________________________________________
@@ -191,6 +205,7 @@
 // }
 
 // 6.
+
 // let tableau = [
 // "Mick",
 // "Patrick",
@@ -210,6 +225,157 @@
 //     }
 // }
 
+// ________________________________________________________________________
+// Exo 3 : Part II 
+
+// 1.
+
+// let tableau = [
+// "Mick",
+// "Patrick",
+// "Homer",
+// "Mehdi",
+// "Witch",
+// "Poulard",
+// "Amen",
+// "Ludovic",
+// "Harry",
+// ]
 
 
+// 2.
 
+// let tableau = [
+// "Mick",
+// "Patrick",
+// "Homer",
+// "Mehdi",
+// "Witch",
+// "Poulard",
+// "Amen",
+// "Ludovic",
+// "Harry",
+// ]
+
+// for (let index = 0; index < tableau.length ; index++) {
+
+//     if (index % 2 == 1) {
+//         console.log(tableau[index].toUpperCase());
+//     }
+// }
+
+
+// 3.
+
+// let tableau = [
+// "Mick",
+// "Patrick",
+// "Homer",
+// "Mehdi",
+// "Witch",
+// "Poulard",
+// "Amen",
+// "Ludovic",
+// "Harry",
+// ]
+
+// for (let index = 0; index < tableau.length ; index++) {
+
+//     if (index % 2 == 0) {
+//         console.log(tableau[index].charAt(0).toUpperCase() + tableau[index].substr(1));
+//     }
+// }
+
+// 4.
+
+
+// let tableau = [
+// "Mick",
+// "Patrick",
+// "Homer",
+// "Mehdi",
+// "Witch",
+// "Poulard",
+// "Amen",
+// "Ludovic",
+// "Harry",
+// ]
+
+// for (let index = 0; index < tableau.length ; index++) {
+
+//     if (index % 2 == 1 && tableau[index].length > 4 ) {
+//         console.log(tableau[index]);
+//     }
+// }
+
+// 5. 
+
+// let tableau = [
+// "Mick",
+// "Patrick",
+// "Homer",
+// "Mehdi",
+// "Witch",
+// "Poulard",
+// "Amen",
+// "Ludovic",
+// "Harry",
+// ]
+
+// for (let index = 0; index < tableau.length ; index++) {
+
+//     if (index % 2 == 0 && tableau[index].length > 4) {
+//         console.log(tableau[index].charAt(0).toUpperCase() + tableau[index].substr(1));
+//     }
+// }
+
+// 6.
+
+// let tableau = [
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
+// ]
+
+// // if (['a', 'e', 'm', 'f', 'y', 'n'].includes(prenom[0])) {
+
+// for (let index = 0; index < tableau.length; index++) {
+//     let prenom = 0;
+//     if (tableau[index].charAt(0) == "a" || tableau[index].charAt(0) == "e" || tableau[index].charAt(0) == "m" || tableau[index].charAt(0) == "f" || tableau[index].charAt(0) == "y" || tableau[index].charAt(0) == "n")
+
+
+//     console.log(tableau[index]); 
+// }
+
+
+// 7.
+
+// let tableau = [
+// "mick",
+// "patrick",
+// "homer",
+// "mehdi",
+// "witch",
+// "poulard",
+// "amen",
+// "ludovic",
+// "harry",
+// ]
+
+// for (let index = 0; index < tableau.length; index++) {
+//     let prenom = 0;
+//     if (tableau[index].length > 5 && tableau[index].charAt(0) == "a" || tableau[index].charAt(0) == "e" || tableau[index].charAt(0) == "m" || tableau[index].charAt(0) == "f" || tableau[index].charAt(0) == "y" || tableau[index].charAt(0) == "n") {
+//         console.log(tableau[index]);
+//     }
+// }
+
+
+// Exo 3 : Part III
+
+// 1.
