3d65aeb79399c3ca9a451849854d94a8735f60d0
unknown <mai.nghia9@gmail.com>
2021-02-10 12:38:54 +0100

    debut codePital

---
+++ JavascriptCodePital/public/js/main.js
@@ -0,0 +1,26 @@
+class malade {
+    constructor(nom,maladie,argent,poche,etatSante,traitement) {
+        this.nom = nom;
+        this.maladie = maladie;
+        this.argent = argent;
+        this.poche = poche;
+        this.etatSante = etatSante;
+        this.traitement = traitement;
+        this.goTo = () => {
+
+        }
+        this.takeCare = () => {
+
+        }
+        this.paye = () => {
+
+        }
+    }
+}
+
+let marcus = new malade("marcus","mal indenté",100,"vide","malade","vide");
+let optimus = new malade("optimus","unsave",200,"vide","malade","vide");
+let sangoku = new malade("sangoku","404",80,"vide","malade","vide");
+let darthvader = new malade("darthvader","azmatique",110,"vide","malade","vide");
+let semicolon = new malade("semicolon","syntaxError",60,"vide","malade","vide");
+
